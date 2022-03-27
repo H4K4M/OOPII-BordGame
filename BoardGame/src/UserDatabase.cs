0f61c0aa586061849fde91815d92ffe2197ec504
Eren Daş <erendasss@gmail.com>
2022-03-27 19:16:23 +0300

    Add User login functionality

---
+++ BoardGame/src/UserDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGame.src
+{
+    class UserDatabase
+    {
+        private Dictionary<String, String> InMemoryUserDatabase = new Dictionary<String, String>
+        {
+            // Username - Password
+
+            { "admin", "admin" },
+            { "user", "user" }
+        };
+
+        public bool IsUserExistsWithUsernameAndPassword(String username, String password)
+        {
+            String userPassword = InMemoryUserDatabase[username];
+
+            if (userPassword.Equals(password))
+            {
+                return true;
+            }
+
+            throw new ArgumentException("Username and password did not matched.");
+        }
+    }
+}
